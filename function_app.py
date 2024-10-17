35618a08fed1679dcb37e01da2f9eeade34c00a8
Longji Kang <kanglongjidev@gmail.com>
2024-10-17 21:42:26 +0200

    Modify function app return statements to account for verification function app running after this function app

--- function_app.py
+++ function_app.py
@@ -20,12 +20,12 @@ def longji_research_create(req: func.HttpRequest) -> func.HttpResponse:
     logging_obj = json.loads(data_string)
 
     f_name = logging_obj["{FilenameWithExtension}"]
-    f_id = logging_obj["{DriveItemId}"]
+    f_id = logging_obj["ID"]
     
     del data_string
     del logging_obj
 
-    logging.info(f"{LOGGING_PREFIX}Evidence gather process started for file creation of {f_name} with id {f_id}...")
+    logging.info(f"{LOGGING_PREFIX}Evidence gather process started for file creation of {f_name} with file id {f_id}...")
     info = proc.extract_metadata(req, ACTION_TYPE.CREATE)
     logging.info(f"{LOGGING_PREFIX}Evidence gather process completed...")
 
@@ -33,9 +33,9 @@ def longji_research_create(req: func.HttpRequest) -> func.HttpResponse:
     evidence_db.create_record(info["blob_name"], info["evidence_hash"], info["encrypted_evidence_hash"])
     logging.info(f"{LOGGING_PREFIX}Storing evidence file information on evidence linking database...")
     db.create_record(info["file_id"], info["blob_name"], info["file_path"], info["directory"], info["file_name"], info["hash"])
-
+    
     return func.HttpResponse(
-        f'success',
+        json.dumps({'evidence_file_name': info['blob_name']}),
         status_code = 200
     )
 
@@ -46,7 +46,7 @@ def longji_research_modified(req: func.HttpRequest) -> func.HttpResponse:
     logging_obj = json.loads(data_string)
 
     f_name = logging_obj["{FilenameWithExtension}"]
-    f_id = logging_obj["{DriveItemId}"]
+    f_id = logging_obj["ID"]
     
     del data_string
     del logging_obj
@@ -60,19 +60,31 @@ def longji_research_modified(req: func.HttpRequest) -> func.HttpResponse:
     db.update_record(info["file_id"], info["blob_name"], info["file_path"], info["directory"], info["file_name"], info["hash"])
 
     return func.HttpResponse(
-        f'success',
+        json.dumps({'evidence_file_name': info['blob_name']}),
         status_code = 200
     )
 
 @app.function_name(name="longji-research-delete")
 @app.route(route="")
 def longji_research_delete(req: func.HttpRequest) -> func.HttpResponse:
+    data_string = proc.convert_request(req)
+    logging_obj = json.loads(data_string)
+
+    f_name = logging_obj["FileNameWithExtension"]
+    f_id = logging_obj["ID"]
+    
+    del data_string
+    del logging_obj
+
+    logging.info(f"{LOGGING_PREFIX}Evidence gather process started for file deletion of {f_name} with file id {f_id}...")
     info = proc.process_deleted(req)
 
+    logging.info(f"{LOGGING_PREFIX}Storing processed evidence on evidence database...")
     evidence_db.create_record(info["blob_name"], info["evidence_hash"], info["encrypted_evidence_hash"])
+    logging.info(f"{LOGGING_PREFIX}Storing evidence file information on evidence linking database...")
     db.update_record(info["file_id"], info["blob_name"], "DELETED", "DELETED", "DELETED", "DELETED")
 
     return func.HttpResponse(
-        f'success',
+        json.dumps({'evidence_file_name': info['blob_name']}),
         status_code = 200
     )
